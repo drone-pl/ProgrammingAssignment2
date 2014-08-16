38ccc87eb768175de0d3606d495998dfd43f2bdf
Igor Gritsenko <xenomorph@thoradin.no-ip.org>
2014-08-16 05:06:46 +0300

    * It's 5 AM and I'm not going to struggle any more to imagine commit message

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##
+## This function creates a special "vector" of functions.
+##
 makeCacheMatrix <- function(x = matrix()) {
 
+    # Here we initialize cache variable.
+    m <- NULL
+
+    # This setter function clears cached variable and sets the original matrix.
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    # This returns original matrix.
+    get <- function() x
+
+    # This sets original matrix without clearing of cached variable.
+    setmatrix <- function(mx) m <<- mx
+
+    # This returns inverted matrix.
+    getmatrix <- function() m
+
+    # List (or "special" vector) of functions that is returned.
+    list(set = set, get = get,
+         setmatrix = setmatrix,
+         getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
-
+##
+## This function inverts the matrix or returns cached matrix if it exists.
+##
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    # Try to get cached inverted matrix.
+    mmatrix <- x$getmatrix()
+
+    # mmatrix isn't null means we already have cached variable, we inform user
+    # about that and returning cached value.
+    if(!is.null(mmatrix)) {
+        message("getting cached data")
+        return(mmatrix)
+    }
+
+    # We didn't find cached matrix, hard work is required.
+    # First we're getting matrix from our beloved "special" vector ("obsv")
+    # that isn't vector but a list.
+    data <- x$get()
+
+    # now we try to solve (a.k.a invert) matrix with provided data.
+    # I assumed that I'll get only square matrix here.
+    # I'm  too lazy to check if it's not.
+    mmatrix <- solve(data,...)
+
+    # Seems like we've solved matrix without any error.
+    # It's time to cache the result by putting it into "obsv"
+    x$setmatrix(mmatrix)
+
+    # At last! Here is your inverted matrix.
+    # You shouldn't complain if you tried to invert uninvertable matrix.
+    # No refunds!
+    mmatrix
 }
+
+
+# I understand that I have a strange sense of humor, hope you enjoyed it.
+# Even if you didn't.
+# Try to not cache your points when you evaluate my work.
